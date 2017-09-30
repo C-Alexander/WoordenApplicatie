9104b2b64d6280d120503479baa35403336f864e
Alexander <alexander@teamcorgi.com>
2017-09-30 23:52:33 +0200

    has some awesome pieces of code I have to remove ;"(

--- src/woordenapplicatie/string/Tool.scala
+++ src/woordenapplicatie/string/Tool.scala
@@ -10,14 +10,6 @@ import scala.collection.mutable
 object Tool {
   private val REGEX:String = "\\, |\\. |\\.\n\n|\\.\n| |\n\n|\n|\\."
 
-  def occurences(text:String): (Int, Int) = {
-//  var map:HashMap[String, AtomicInteger] = new mutable.HashMap[String, AtomicInteger]()
-//  text.split(Array('\n', ',', ' ')).foreach(s =>  map.getOrElseUpdate(s.toLowerCase, new AtomicInteger(0)).incrementAndGet())
-  var s:String = new String()
-   var strs:Array[String] = text.toLowerCase.split("\\, | |\n\n|\n|\\.")
-   (strs.length, strs.toSet.size)
-
-}
   /**
     *
     * @param text text to count words of
@@ -29,23 +21,29 @@ object Tool {
   }
 
   def sortAlphabetically(text:String): String = {
-//    var words:SortedSet[String] = new SortedSet[String](text.toLowerCase.split("\\, |\\. |\\.\n\n|\\.\n| |\n\n|\n|\\."))
-    //   text.toLowerCase.split("\\, |\\. |\\.\n\n|\\.\n| |\n\n|\n|\\.").to[SortedSet].toString() cool!
-   // text.toLowerCase.split("\\, |\\. |\\.\n\n|\\.\n| |\n\n|\n|\\.").toSet.
     (SortedSet[String]()(Ordering[String].reverse) ++ text.toLowerCase.split(REGEX)).mkString("\n")
+    //    var words:SortedSet[String] = new SortedSet[String](text.toLowerCase.split("\\, |\\. |\\.\n\n|\\.\n| |\n\n|\n|\\."))
+    //   text.toLowerCase.split("\\, |\\. |\\.\n\n|\\.\n| |\n\n|\n|\\.").to[SortedSet].toString() cool!
+    // text.toLowerCase.split("\\, |\\. |\\.\n\n|\\.\n| |\n\n|\n|\\.").toSet.
   }
 
   def frequence(text:String): String = {
-      var map:mutable.HashMap[String, AtomicInteger] = new mutable.HashMap[String, AtomicInteger]()
-      text.split(REGEX).foreach(s =>  map.getOrElseUpdate(s.toLowerCase, new AtomicInteger(0)).incrementAndGet())
-    println(map.toString())
-      var sortedMap:mutable.SortedMap[Int, ListBuffer[String]] = mutable.SortedMap[Int, ListBuffer[String]]()
- //   map.foreach(m => sortedMap += m._2.intValue() -> m._1)
-    sortedMap += 1 -> ListBuffer("ok!")
-    sortedMap(1) += "kay"
-    map.foreach(m => if(sortedMap.contains(m._2.intValue())) sortedMap.get(m._2.intValue()) ++= "" else sortedMap += m._2.intValue() -> ListBuffer(m._1))
-    sortedMap.mkString("\n")
-//    (SortedSet[Int]()(Ordering[Int]) ++ map).mkString("\n")
-//var map:SortedMap[String, AtomicInteger] = SortedMap[String, AtomicInteger]()(Ordering[Int])
+    val map:mutable.HashMap[String, AtomicInteger] = new mutable.HashMap[String, AtomicInteger]()
+    text.toLowerCase.split(REGEX).foreach(s =>  map.getOrElseUpdate(s.toLowerCase, new AtomicInteger(0)).incrementAndGet())
+
+    val sortedMap:mutable.SortedMap[Int, ListBuffer[String]] = mutable.SortedMap[Int, ListBuffer[String]]()
+    map.foreach(m => sortedMap.getOrElseUpdate(m._2.intValue(), ListBuffer()) += (m._2.toString + "\t" + m._1 + "\n"))
+
+     sortedMap.foldLeft(new StringBuilder) {(s, m) => s.append(m._2.mkString(""))}.toString() //m._2.mkString(s"\t ${m._1} \n") + "\n") pretty, but flawed
+   // sortedMap.foreach(m => m._1.toString)
+
+
+    //   map.foreach(m => sortedMap += m._2.intValue() -> m._1)
+    //    sortedMap += 1 -> ListBuffer("ok!")
+    //    sortedMap(1) += "kay"
+    //    if (sortedMap.contains(1)) sortedMap.get(1).head += "kay" else sortedMap += 1 -> ListBuffer("as")
+    //    map.foreach(m => if(sortedMap.contains(m._2.intValue())) println(sortedMap.get(m._2.intValue()).head) else sortedMap += m._2.intValue() -> ListBuffer(m._1))
+    //    (SortedSet[Int]()(Ordering[Int]) ++ map).mkString("\n")
+    //var map:SortedMap[String, AtomicInteger] = SortedMap[String, AtomicInteger]()(Ordering[Int])
   }
 }
