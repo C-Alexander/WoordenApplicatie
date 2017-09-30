8d70314d9c160b67830709ece94ab10190af4f8f
C-Alexander <alexander@teamcorgi.com>
2017-09-30 16:04:03 +0200

    stuff

--- src/woordenapplicatie/string/Tool.scala
+++ src/woordenapplicatie/string/Tool.scala
@@ -3,9 +3,9 @@ package woordenapplicatie.string
 import java.util
 import java.util.concurrent.atomic.AtomicInteger
 
-import scala.collection.SortedMap
+import scala.collection.mutable.{HashMap, ListBuffer}
 import scala.collection.immutable.SortedSet
-import scala.collection.mutable.HashMap
+import scala.collection.mutable
 
 object Tool {
   private val REGEX:String = "\\, |\\. |\\.\n\n|\\.\n| |\n\n|\n|\\."
@@ -36,9 +36,16 @@ object Tool {
   }
 
   def frequence(text:String): String = {
-//      var map:HashMap[String, AtomicInteger] = new HashMap[String, AtomicInteger]()
-//      text.split(REGEX).foreach(s =>  map.getOrElseUpdate(s.toLowerCase, new AtomicInteger(0)).incrementAndGet())
+      var map:mutable.HashMap[String, AtomicInteger] = new mutable.HashMap[String, AtomicInteger]()
+      text.split(REGEX).foreach(s =>  map.getOrElseUpdate(s.toLowerCase, new AtomicInteger(0)).incrementAndGet())
+    println(map.toString())
+      var sortedMap:mutable.SortedMap[Int, ListBuffer[String]] = mutable.SortedMap[Int, ListBuffer[String]]()
+ //   map.foreach(m => sortedMap += m._2.intValue() -> m._1)
+    sortedMap += 1 -> ListBuffer("ok!")
+    sortedMap(1) += "kay"
+    map.foreach(m => if(sortedMap.contains(m._2.intValue())) sortedMap.get(m._2.intValue()) ++= "" else sortedMap += m._2.intValue() -> ListBuffer(m._1))
+    sortedMap.mkString("\n")
 //    (SortedSet[Int]()(Ordering[Int]) ++ map).mkString("\n")
-var map:SortedMap[String, AtomicInteger] = SortedMap[String, AtomicInteger]()(Ordering[Int])
+//var map:SortedMap[String, AtomicInteger] = SortedMap[String, AtomicInteger]()(Ordering[Int])
   }
 }
