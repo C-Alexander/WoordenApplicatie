d2a73488a16c4404a1e61ccbd1b1a8e2882c0c60
C-Alexander <alexander@teamcorgi.com>
2017-10-02 12:13:57 +0200

    stuff

--- src/woordenapplicatie/string/Tool.scala
+++ src/woordenapplicatie/string/Tool.scala
@@ -17,7 +17,6 @@ object Tool {
     * @return tuple containing (count, countUnique) as ints.
     */
   def count(text:String): (Int, Int) = {
-
     time.measureTime {
       val strs: Array[String] = text.toLowerCase.split(REGEX)
       (strs.length, strs.toSet.size)
@@ -32,8 +31,8 @@ object Tool {
 
   def frequence(text:String): String = {
     time.measureTime {
-      val map: mutable.HashMap[String, AtomicInteger] = mutable.HashMap[String, AtomicInteger]() //sadly has to be mutable if we want dont want to manually count every word later...
-      text.toLowerCase.split(REGEX).foreach(s => map.getOrElseUpdate(s, new AtomicInteger(0)).incrementAndGet()) //record occurence of each word
+      val map: mutable.HashMap[String, AtomicInteger] = mutable.HashMap[String, AtomicInteger]() //sadly has to be mutable if we want don't want to manually count every word later...
+      text.toLowerCase.split(REGEX).foreach(s => map.getOrElseUpdate(s, new AtomicInteger(0)).incrementAndGet()) //record occurrence of each word
       // AtomicInteger method results for 290000 words: 195, 100, 104, 101, 113, 135, standard put results: 288, 232, 236, 204, 265, 262. Winner winner chicken dinner!
 
       val sortedMap: mutable.SortedMap[Int, ListBuffer[String]] = mutable.SortedMap[Int, ListBuffer[String]]()
@@ -44,29 +43,28 @@ object Tool {
   }
 
   def concordance(text:String): String = {
-    val map:mutable.HashMap[String, mutable.SortedSet[Int]] = new mutable.HashMap[String, mutable.SortedSet[Int]]
-    val word:mutable.StringBuilder = new mutable.StringBuilder() //5.1 is the average amount of letters per world, is it too insane to make this the initial size and make the normal increase the deviation?
-    var line:Int = 1
+    time.measureTime {
+      val map: mutable.HashMap[String, mutable.SortedSet[Int]] = new mutable.HashMap[String, mutable.SortedSet[Int]]
+      val word: mutable.StringBuilder = new mutable.StringBuilder() //5.1 is the average amount of letters per world, is it too insane to make this the initial size and make the normal increase the deviation?
+      var line: Int = 1
 
-    for (char <- text + ',') {
-      char match {
-        case '\n' | ',' | ' ' | '.' =>
-          if (word.nonEmpty) {
-            map.getOrElseUpdate(word.toString(), mutable.SortedSet[Int]()) += line
-            word.clear()
-          }
-          if (char == '\n') line += 1
-        case letter => word append letter
+      for (char <- text + ',') {
+        char match {
+          case '\n' | ',' | ' ' | '.' =>
+            if (word.nonEmpty) {
+              map.getOrElseUpdate(word.toString(), mutable.SortedSet[Int]()) += line
+              word.clear()
+            }
+            if (char == '\n') line += 1
+          case letter => word append letter
+        }
       }
+      word.clear()
+      for (x <- map) {
+        word.append(x._1 + '\t' + x._2.mkString("[", ", ", "]" + '\n'))
+      }
+      word.toString()
     }
-    word.clear()
-    for (x <- map) {
-      word.append(x._1 + '\t' + x._2.mkString("[", ", ", "]" + '\n'))
-    }
-    word.toString()
   }
-
-
-
     //overriding a set is easy, just use setproxy! Sadly now deprecated. Damn you, BDFL Odersky!
 }
